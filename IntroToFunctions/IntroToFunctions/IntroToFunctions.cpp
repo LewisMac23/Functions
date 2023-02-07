11d591b730d4830f9a94190b683fb7b619707877
LewisMac23 <124689702+LewisMac23@users.noreply.github.com>
2023-02-07 13:48:37 +0000

    Add files via upload

---
+++ IntroToFunctions/IntroToFunctions/IntroToFunctions.cpp
@@ -0,0 +1,25 @@
+// IntroToFunctions.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//Example to the Power of function
+
+pow(10, 2)
+
+//Data in brackets is known as the ARGUMENT, often more than one - here it is 10(arg1) to the power of 2(arg2)
+//ARGUMENTS are the input POW is the machine and the output is the "RETURN"
+//If function dosen't necesarilly give a value back use ";" for RETURN
+//When assigning function to a variable, e.g "x" however do the below;
+
+x = pow(10, 2);
+
+//When you want to manipulate the function and return VALUES, e.g "func" mukltiplied by 2;
+
+x = pow(10, 2) * 2;
+//can also do this as 
+x = x*2
+
+//can use cout to output the entire result (using namespace std)
+
+cout << x;
+
+//The "POWER" function returns a DOUBLE - A number with decimal values after
